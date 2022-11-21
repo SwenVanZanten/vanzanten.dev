77450f9ece38b387ff62e3312f2bcdf7b2208f7a
Swen van Zanten <info@hdsign.nl>
2022-11-21 10:11:49 +0100

    add favicon and html metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     ssr: false,
+
     modules: [
         '@nuxtjs/tailwindcss'
-    ]
+    ],
+
+    app: {
+        head: {
+            title: 'vanzanten.dev',
+            meta: [
+                { name: 'description', content: 'company website of vanzanten.dev' }
+            ],
+            link: [
+                { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+                { rel: 'icon', sizes: '32x32', href: '/favicon-32x32.png', type: 'image/png' },
+                { rel: 'icon', sizes: '16x16', href: '/favicon-16x16.png', type: 'image/png' },
+                { rel: 'manifest', href: '/site.webmanifest' }
+            ]
+        }
+    }
 })
